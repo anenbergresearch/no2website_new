ca09156f177c3f339c76fcf7e5cacaf254097181
Anenberg Research <104150443+anenbergresearch@users.noreply.github.com>
2026-03-17 15:10:39 -0400

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // Enables static HTML export
+  output: 'export',
+  
+  // Necessary if your repo name is something like 'NO2website' and not a user/org page.
+  basePath: isProd ? '/no2website_new' : '',
+  assetPrefix: isProd ? '/no2website_new/' : '',
 
+  // Optional: Disable image optimization if you are relying on standard <img> tags
+  images: {
+    unoptimized: true,
+  },
+};
 export default nextConfig;
